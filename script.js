cb0062204aeea065de8ccdf9f4749030fd58fee0
gabriel31314124-ctrl <gabriel31314124@gmail.com>
2026-02-08 00:48:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let tg = window.Telegram.WebApp;
 tg.ready();
+
 let user = tg.initDataUnsafe.user;
 document.getElementById("user").innerText = "Salam, " + user.first_name;
+
 let score = 0;
-function addPoint() {
-  score++;
-  document.getElementById("score").innerText = score;
+const pixel = document.getElementById("pixel");
+const gameArea = document.getElementById("game-area");
+
+function clickPixel() {
+    score++;
+    document.getElementById("score").innerText = score;
+    movePixel();
+}
+
+function movePixel() {
+    const maxX = gameArea.clientWidth - pixel.clientWidth;
+    const maxY = gameArea.clientHeight - pixel.clientHeight;
+
+    const newX = Math.floor(Math.random() * maxX);
+    const newY = Math.floor(Math.random() * maxY);
+
+    pixel.style.left = newX + "px";
+    pixel.style.top = newY + "px";
 }
